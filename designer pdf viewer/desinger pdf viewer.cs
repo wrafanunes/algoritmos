667627487aacfe9f25ddfaa97f6aebdb6982622c
Wilson Nunes <wrsnunes@gmail.com>
2023-08-14 12:11:36 -0300

    implementa a solução do algoritmo design pdf viewer

---
+++ designer pdf viewer/desinger pdf viewer.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        int tallestLetter = 0;
+        for(int i = 0; i < word.Length; i++){
+            int position = char.ToLower(word.ElementAt(i)) - 'a' + 1;
+            if(h.ElementAt(position - 1) > tallestLetter){
+                tallestLetter = h.ElementAt(position - 1);
+            }
+        }
+        return tallestLetter * word.Length;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
